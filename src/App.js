46ba71d1e985b6f35b6a72a5564e322264e2df48
Kervens Jasmin <jakejasminjk@gmail.com>
2020-09-10 13:01:44 -0400

    Packages
    Packages changed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import DataT from './components/DataT';
+import SignIn from './components/Sign'
+import Show from './components/Show'
+import CssBaseline from '@material-ui/core/CssBaseline';
+//import Link from '@material-ui/core/Link';
+//import Container from '@material-ui/core/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {stateVal: 'NONE'};
+
+    this.handleState = this.handleState.bind(this);
+  }
+  handleState(state, stateNum){
+    this.setState({ stateVal: state}, () => {
+    console.log(this.state.stateVal, 'state is set');
+  });
+  }
+
+  render(){
+    return (
+      <Router>
+        <React.Fragment>
+          <CssBaseline />
+          <Nav name="Contact Tracing America"/>
+          <Route exact path="/" render={props =>(
+          <Main />
+          )}/>
+          <header>
+          </header>
+          <Route path="/info" render={props =>(
+          <React.Fragment>
+          <SignIn stateVal= {this.state.stateVal} handleState={this.handleState}/>
+          </React.Fragment>
+          )}/>
+          <Route path="/info/rev" render={props =>(
+          <React.Fragment>
+          <Show stateVal= {this.state.stateVal}/>
+          </React.Fragment>
+          )}/>
+          <Route path="/data" render={props =>(
+          <React.Fragment>
+          <DataT />
+          </React.Fragment>
+          )}/>
+      </React.Fragment>
+     </Router>
+  )};
 }
 
 export default App;
