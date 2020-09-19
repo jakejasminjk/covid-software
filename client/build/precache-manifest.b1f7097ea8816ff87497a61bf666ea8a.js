53f48d98d1d1eed4f7f6fd1308f96d513e40614e
Kervens Jasmin <jakejasminjk@gmail.com>
2020-09-19 09:56:01 -0400

    try 4

---
+++ client/build/precache-manifest.b1f7097ea8816ff87497a61bf666ea8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75368df1da742d5ce0fb7234cf8e043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef9db0427a5f9c508d1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "70b25e352e58279e9037",
+    "url": "/static/js/2.f49342e9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f49342e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef9db0427a5f9c508d1",
+    "url": "/static/js/main.6a3b693c.chunk.js"
+  },
+  {
+    "revision": "b7653115be593b727b08",
+    "url": "/static/js/runtime-main.af499148.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-//Used for rest actions such as get and post
-const express = require('express');
-//Allows us to bypass the cross-origin policy
-const cors = require('cors');
-//Allows us to make models for or database collections
-const mongoose = require('mongoose');
-
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import config from './config';
 //environment variable
 require('dotenv').config();
 
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 //const port = process.env.PORT || 80;
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -26,6 +27,15 @@ connection.once('open', () => {
 const usersRouter = require('./routes/users');
 
 app.use('/users', usersRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
